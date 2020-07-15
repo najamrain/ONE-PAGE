2b402a4664359fad4216df64acd5bb04bb5e4a27
Najam Rain <najamrain@gmail.com>
2020-07-15 17:13:33 +0400

    first commit

---
+++ form.js
@@ -0,0 +1,16 @@
+function focusf(element) {
+  element.style.outline = "none";
+  // element.style.background = "";
+  // element.style.width = "300px";
+  element.style.border = "3px solid blue";
+}
+
+function blurf(element) {
+  element.style.background = "";
+  element.style.border = "";
+  element.style.width = "";
+}
+
+function sub() {
+  alert("Your Fucking Form Is Submitted");
+}
